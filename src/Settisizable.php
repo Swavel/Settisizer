68fd276a76c299074dd021c4e313db628ec428c7
Philippe Bumann <philippe.bumann@mexan.ch>
2017-04-18 21:27:29 +0200

    basic hardcoded save and read method

--- src/Settisizable.php
+++ src/Settisizable.php
@@ -3,14 +3,40 @@
 namespace Settisizer;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 trait Settisizable {
-    function getMyType() {
+    function setSetting($key, $value) {
+        if($this->settingExists($key) === true) {
+            //do the update thing
+        } else {
+            //do the create thing
+        }
+        //TODO: replace with implementation
 
-        dd([
-            $this->{$this->getKeyName()},
-            $this->getTable(),
-            $this instanceof Model
-        ]);
+        Storage::put('settisizer/'.$this->getKeyString($key,'/'), $value);
+        return true;
+    }
+
+    function getSetting($key) {
+        return $this->settingExists($key)
+            ? Storage::get('settisizer/'.$this->getKeyString($key,'/'))
+            : false;
+    }
+
+    function settingExists($key) {
+        return Storage::exists('settisizer/'.$this->getKeyString($key,'/'));
+    }
+
+    private function getKeyString($key, $separator = '.') {
+        if($this instanceof Model) {
+            return 'eloquentmodel' . $separator .
+                $this->getTable() . $separator .
+                $this->{$this->getKeyName()} . $separator .
+                $key;
+        }
+
+        return 'app' . $separator .
+            $key;
     }
 }
